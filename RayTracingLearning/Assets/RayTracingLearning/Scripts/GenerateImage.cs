242bf54431452475bf8384b55edad6e2210f6eaf
windsmoon <smwindsmoon@gmail.com>
2019-12-04 14:08:50 +0800

    develop generating animation #4

--- RayTracingLearning/Assets/RayTracingLearning/Scripts/GenerateImage.cs
+++ RayTracingLearning/Assets/RayTracingLearning/Scripts/GenerateImage.cs
@@ -27,24 +27,51 @@ namespace RayTracingLearning
         private int maxReflectCount = 50;
         [SerializeField, Range(0f, 1f)]
         private float globalMetalFuzziness = 1f;
+        [SerializeField]
+        private Vector2Int resolution = new Vector2Int(200, 100);
         private Sphere sphere1;
         private Sphere sphere2;
         private Sphere sphere3;
         private Sphere sphere4;
         private List<Geometry> sphereList;
+        private Texture2D texture;
+        private Camera camera;
+        private int currentRow;
+        private int currentCol;
+        private bool isGenerating = false;
         #endregion
         
+        #region unity methods
+        private void Update()
+        {
+            if (isGenerating == false)
+            {
+                return;
+            }
+            
+            Color color = GetColor(camera, texture, currentCol, currentRow);
+            color.R = (float)Math.Sqrt(color.R);
+            color.G = (float)Math.Sqrt(color.G);
+            color.B = (float)Math.Sqrt(color.B);
+            texture.SetPixel(currentCol, currentRow, new UnityEngine.Color(color.R, color.G, color.B, 1f));
+            Debug.Log("generate pixel : (" + currentCol + ", " + currentRow + ") finish");
+            GetNextRowCol();
+            texture.Apply();
+        }
+
+        #endregion
+
         #region methods
         [ContextMenu("Generate Image")]
         private void Generate()
         {
             // build the world, width : 200, height : 100, depth : 100
-            Texture2D texture = new Texture2D(1920, 1080);
+            texture = new Texture2D(resolution.x, resolution.y);
             //Vector3 horizontalLength = new Vector3(4, 0, 0); // width : 200
             //Vector3 verticalLength = new Vector3(0, 2, 0); // height : 100
             //Vector3 center = new Vector3(0, 0, 0);
             //Vector3 lowLeftCorner = new Vector3(-2, -1, 1); // depth : 50, z : 1
-            Camera camera = new Camera(new Vector3(0f, 0f, 0f), 4f, 2f, 1f);
+            camera = new Camera(new Vector3(0f, 0f, 0f), 4f, 2f, 1f);
             Material lambertian1 = new Lambertian(new Color(0.8f, 0.3f, 0.3f));
             Material lambertian2 = new Lambertian(new Color(0.8f, 0.8f, 0f));
             Material metal1 = new Metal(new Color(0.8f, 0.6f, 0.2f), 1f * globalMetalFuzziness);
@@ -54,22 +81,43 @@ namespace RayTracingLearning
             sphere3 = new Sphere(metal1, new Vector3(1f,0f,1f), 0.5f);
             sphere4 = new Sphere(metal2, new Vector3(-1,0,1f), 0.5f);
             sphereList = new List<Geometry>() {sphere1, sphere2, sphere3, sphere4};
-            
-            for (int i = 0; i < texture.width; ++i)
-            {
-                for (int j = texture.height - 1; j >= 0; --j)
-                {
-                    Color color = GetColor(camera, texture, i, j);
-                    color.R = (float)Math.Sqrt(color.R);
-                    color.G = (float)Math.Sqrt(color.G);
-                    color.B = (float)Math.Sqrt(color.B);
-                    texture.SetPixel(i, j, new UnityEngine.Color(color.R, color.G, color.B, 1f));
-                }
-            }
-
             texture.filterMode = FilterMode.Point;
             texture.Apply();
             GetComponent<RawImage>().texture = texture;
+            currentRow = resolution.y - 1;
+            currentCol = 0;
+            isGenerating = true;
+            Debug.Log("generate start");
+            
+            
+        //     for (int i = 0; i < texture.width; ++i)
+        //     {
+        //         for (int j = texture.height - 1; j >= 0; --j)
+        //         {
+        //             // Color color = GetColor(camera, texture, i, j);
+        //             // color.R = (float)Math.Sqrt(color.R);
+        //             // color.G = (float)Math.Sqrt(color.G);
+        //             // color.B = (float)Math.Sqrt(color.B);
+        //             // texture.SetPixel(i, j, new UnityEngine.Color(color.R, color.G, color.B, 1f));
+        //         }
+        //     }
+         }
+
+        private void GetNextRowCol()
+        {
+            ++currentCol;
+
+            if (currentCol == resolution.x)
+            {
+                currentCol = 0;
+                --currentRow;
+
+                if (currentRow == -1)
+                {
+                    isGenerating = false;
+                    Debug.Log("generate finish");
+                }
+            }
         }
         
         private Color GetColor(Camera camera, Texture texture, int x, int y)
