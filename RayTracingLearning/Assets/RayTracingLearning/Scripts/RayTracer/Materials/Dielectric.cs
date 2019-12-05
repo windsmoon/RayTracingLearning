99433a1b9e2a405b409d1912425970d8cb27cce5
windsmoon <smwindsmoon@gmail.com>
2019-12-05 20:35:22 +0800

    finish Ray Tracing in One Weekend #2 add dielectric material

--- RayTracingLearning/Assets/RayTracingLearning/Scripts/RayTracer/Materials/Dielectric.cs
+++ RayTracingLearning/Assets/RayTracingLearning/Scripts/RayTracer/Materials/Dielectric.cs
@@ -1,10 +1,17 @@
+using RayTracingLearning.RayTracer.Math;
+
 namespace RayTracingLearning.RayTracer.Materials
 {
     public class Dielectric : Material
     {
+        #region fields
+        private float refractRatio;
+        #endregion
+    
         #region constructors
-        public Dielectric(Color albedo) : base(albedo)
+        public Dielectric(Color albedo, float refractRatio) : base(albedo)
         {
+            this.refractRatio = refractRatio;
         }
         #endregion
 
@@ -19,8 +26,17 @@ namespace RayTracingLearning.RayTracer.Materials
             throw new System.NotImplementedException();
         }
         
-        private bool Refract(Ray rayIn)
+        private bool Refract(Ray rayIn, HitInfo hitInfo, out Ray rayOut)
         {
+            float cosInAngle = Vector3.Dot(rayIn.Direction, -1 * hitInfo.Normal);
+            float squaredSinInAngle = 1 - cosInAngle * cosInAngle;
+            float squaredCosIOutAngle = 1 - refractRatio * refractRatio * squaredSinInAngle;
+
+            if (squaredCosIOutAngle > 0) // todo how can be less than 0 ?
+            {
+                
+            }
+            
             return false;
         }
         #endregion
